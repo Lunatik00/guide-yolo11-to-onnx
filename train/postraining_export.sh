e9073f35aeb72ff762fc254a6574994273fd7cf3
Alen Asenie <alen.asenie@gmail.com>
2025-02-11 16:11:42 -0300

    remove some unnecesary files from the project.

--- train/postraining_export.sh
+++
@@ -1,2 +0,0 @@
-uv run export.py ./medical_pills/yolo11n/weights/best.pt
-cp ./medical_pills/yolo11n/weights/best.onnx ../yolo11n_medical_pills.onnx
--- train/train_container.sh
+++
@@ -1,2 +0,0 @@
-podman build . -t train-yolo11
-podman run  --rm -it --device nvidia.com/gpu=all --shm-size=4gb -v $PWD:/code train-yolo11
